0f17bed6e873c7ca04f4b734b788f405b6fe6d7e
Tom Scott-Malden <tom@skywire.co.uk>
2017-07-05 14:13:06 +0100

    initial commit - Symfony installed

---
+++ README.md
@@ -0,0 +1,4 @@
+Address Book
+=========
+
+A test symfony project to familiarise mysql with the framework in a setting that i'm familar with.
---
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Address Book', $crawler->filter('h1')->text());
+	    $this->assertContains('List', $crawler->filter('h2')->text());
+    }
+}
